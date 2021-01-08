fc9215c6cb90ebc5413cd22824573322d38797fc
monax-kun <monawasensei@gmail.com>
2021-01-07 22:11:40 -0600

    adding fanart directory with index.php

---
+++ archive/fanart/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>pics main page</title>
+	<style>
+		* {
+			max-height: 200px;
+		}
+	</style>
+</head>
+<body>
+	<div>
+		<?php 
+			$conn = new mysqli("monawasensei56192.domaincommysql.com", "tuber", "Test123!@#","potato_database"); 
+			if ($conn->connect_error) { 
+				die('Could not connect: ' . $conn->connect_error); 
+			} 
+		$sql = "SELECT CONCAT(\"https://holedigging.club/archive/fanart/fanart/\",filename) AS 'absDir' FROM archive_pics";
+		$result = $conn->query($sql);
+		
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				$filename = $row["absDir"];
+				echo "<a href=\"${filename}\" target=\"_blank\"> <image src=\"${filename}\"></a>";
+			"<br>";
+			}
+		} 
+		else {
+			echo "\n0 results";
+		}
+		?>
+	</div>
+	
+	<div id = "imageDiv">
+	</div>
+	
+	<div id = "loadMoreDiv">
+	</div>
+</body>
+</html>
