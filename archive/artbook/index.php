45e6797fe734520fc002ee0eaeaca642eae37acc
monawasensei <75004614+monawasensei@users.noreply.github.com>
2021-01-07 20:57:00 -0600

    Update index.php
    attempting to echo anchor tags

--- archive/artbook/index.php
+++ archive/artbook/index.php
@@ -17,7 +17,7 @@
 		if ($result->num_rows > 0) {
 			while($row = $result->fetch_assoc()) {
 				$filename = $row["absDir"];
-				echo $filename;
+				echo "<a href=\"${filename}\"> <image src=\"${filename}\"></a>";
 			"<br>";
 			}
 		} 
