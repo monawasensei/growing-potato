22d4a82a3c0990f1bd0ef9db3855550222326845
monawasensei <75004614+monawasensei@users.noreply.github.com>
2021-01-07 21:42:17 -0600

    Update index.php

--- archive/artbook/index.php
+++ archive/artbook/index.php
@@ -2,11 +2,6 @@
 <html>
 <head>
 <title>artbook main page</title>
-	<style>
-	image {
-		max-height: 300px;
-	}
-	</style>
 </head>
 <body>
 	<div>
@@ -21,7 +16,7 @@
 		if ($result->num_rows > 0) {
 			while($row = $result->fetch_assoc()) {
 				$filename = $row["absDir"];
-				echo "<a href=\"${filename}\" target=\"_blank\"> <image src=\"${filename}\"></a>";
+				echo "<a href=\"${filename}\" target=\"_blank\"> <image src=\"${filename}\" style=\"maxHeight:300px\"></a>";
 			"<br>";
 			}
 		} 
