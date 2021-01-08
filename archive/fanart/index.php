f80430c7d0e188ed71a5cdca9af41808f345855f
monawasensei <75004614+monawasensei@users.noreply.github.com>
2021-01-07 22:52:42 -0600

    Update index.php
    changed the query table because I'm a literal fucking retard

--- archive/fanart/index.php
+++ archive/fanart/index.php
@@ -15,7 +15,7 @@
 			if ($conn->connect_error) { 
 				die('Could not connect: ' . $conn->connect_error); 
 			} 
-		$sql = "SELECT CONCAT(\"https://holedigging.club/archive/fanart/fanart/\",filename) AS 'absDir' FROM archive_pics";
+		$sql = "SELECT CONCAT(\"https://holedigging.club/archive/fanart/fanart/\",filename) AS 'absDir' FROM archive_fanart";
 		$result = $conn->query($sql);
 		
 		if ($result->num_rows > 0) {
