4e49664aa6d37765d6f8097a40d2b41fcce88f8f
monax-kun <monawasensei@gmail.com>
2021-01-08 23:41:39 -0600

    Adding screencaps php and removing its old placeholder html, basically a copy of pic.php but taking advantage of the new mysql table structure

---
+++ archive/screencaps/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>screencaps main page</title>
+	<style>
+		* {
+			max-height: 300px;
+		}
+	</style>
+</head>
+<body>
+	<div>
+		<?php 
+			$conn = new mysqli("monawasensei56192.domaincommysql.com", "tuber", "Test123!@#","potato_database"); 
+			if ($conn->connect_error) { 
+				die('Could not connect: ' . $conn->connect_error); 
+			} 
+		$sql = "SELECT CONCAT(absolutepath,CASE WHEN relativepath IS NULL THEN \'\' ELSE relativepath END,filename) from screencaps where image_id < 21";
+		$result = $conn->query($sql);
+		
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				$filename = $row["absDir"];
+				echo "<a href=\"${filename}\" target=\"_blank\"> <image src=\"${filename}\"></a>";
+			"<br>";
+			}
+		} 
+		else {
+			echo "\n0 results";
+		}
+		?>
+	</div>
+	
+	<div id = "imageDiv">
+	</div>
+	
+	<div id = "loadMoreDiv">
+	</div>
+</body>
+</html>
