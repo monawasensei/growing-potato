fd6b03686d739b6363d7023555164469e481515b
monawasensei <75004614+monawasensei@users.noreply.github.com>
2021-01-09 11:46:16 -0600

    Update index.php
    adding style sheet ref

--- archive/pics/index.php
+++ archive/pics/index.php
@@ -2,11 +2,7 @@
 <html>
 <head>
 <title>pics main page</title>
-	<style>
-		* {
-			max-height: 300px;
-		}
-	</style>
+<link rel="stylesheet" href="https://holedigging.club/archive/archiveStyle.CSS">
 </head>
 <body>
 	<div>
