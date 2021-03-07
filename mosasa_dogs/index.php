d5e78bf3cf4ceb1d5071e131b19afcb04a5a69e5
monax-kun <monawasensei@gmail.com>
2021-03-06 21:07:16 -0600

    copied index.html to index.php as I suspect I will need to use that to read the log, also made some changes and additions to queue-handler.js, the next piece of the puzzle is reading the queue and parsing it.

---
+++ mosasa_dogs/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Radio</title>
+	<link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+	<h1>
+	Future home of the Mosasa Dogs!
+	</h1>
+	<div id="player-toplevel" class="player-div">
+		<iframe id="player" width="560" height="315" src="https://www.youtube.com/embed/FcZOnrL9VKM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<div id="queue" class="player-div">
+			<p id="queue-test-text">
+			How do I get this thing to stay in the middle?
+			</p>
+			<button type="button" onclick="button_test()">desd sgribd :DD</button>
+		</div>
+	</div>
+<script src="queue_handler.js"></script> <!-- have no idea if I need an end tag for an external script. Can't hurt I guess -->
+<noscript>why don'd u hab jabasgribd durnd on? :DDDDDDDDDDDDDDDD</noscript>
+</body>
+</html>
--- mosasa_dogs/queue_handler.js
+++ mosasa_dogs/queue_handler.js
@@ -1,23 +1,55 @@
+//this will need to handle url's from youtube, and soundcloud, potentially raw videos too.
+//for now, youtube is enough
+
 var queueIndex = new Array();
+var queueEntryId = 0
+var log = new Array();
 
 class queueEntry {
 	constructor(src) {
 		this.src = src;
-		this.create_entry_element()
+		queueIndex.push(this);
+		this.create_entry();
+		this.add_log_info(); //wip
 	}
-	create_entry_element() {
+
+	create_entry() {
+		this.create_entry_div()
+		this.populate_entry_div()
+	}
+
+	remove_entry() {
+		var index = queueIndex.indexOf(this)
+		queueIndex.splice(index,1)
+	}
+
+	create_entry_div() {
+		queueEntryId += 1
 		var queueContainer = document.getElementById("queue")
 		this.entryDiv = document.createElement("div")
-		//need to assign a class or id to the div here but I think it's fine to ignore for now.
+		this.entryDivId = "entry-" + queueEntryId
+		this.entryDiv.setAttribute("id",this.entryDivId)
+		this.entryDiv.setAttribute("class","entry-div")
 		queueContainer.appendChild(this.entryDiv)
+	}
 
+	populate_entry_div() {
 		this.entryTitle = document.createElement("p")
 		this.entryTitle.setAttribute("class","entry-title")
 		var entryTitleText = document.createTextNode(this.src) //will have to change this to get the video title instead of the url I guess
+		//How to get video title from youtube url??
+			//will have to add some youtube webAPI to do that..
 		this.entryTitle.appendChild(entryTitleText)
-
 		this.entryDiv.appendChild(this.entryTitle)
 	}
+
+	add_log_info() {
+		//wip
+	}
+}
+
+function get_log() {
+	//open log and load each line to an array(Array log) or something
 }
 
 
@@ -26,8 +58,22 @@ function make_div_test() {
 }
 
 function button_test() {
-	document.getElementById("queue-test-text").innerHTML = "desting jabasgribd :DDDDDDDDDDD"
+	document.getElementById("queue-test-text").innerHTML = "desting jabasgribd :DDDDDDDDDDD";
 	make_div_test()
 }
 
+function play_queue() {
+	play_next_entry()
+	//queueIndex.shift();
+
+}
+
+function play_next_entry() {
+	var nextEntryObject = queueIndex[0];
+	player = document.getElementById("player");
+	player.setAttribute("src",nextEntryObject.src);
+	//nextEntryObject.remove_entry();
+}
+
+//don't know if I need to add an update_queue() function, but I will see
 
