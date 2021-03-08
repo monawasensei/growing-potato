be21f37551f73f0a0fce331be44e3d9c50707e15
monax-kun <monawasensei@gmail.com>
2021-03-08 00:26:39 -0600

    changing some styling for the log page

--- mosasa_dogs/log_display.php
+++ mosasa_dogs/log_display.php
@@ -2,6 +2,23 @@
 <head>
 <title>Potatofields playlist log</title>
 <meta charset="utf-8">
+<link rel="stylesheet" href="https://holedigging.club/potatofieldsData/potatofields.css"><style>
+  a {
+    color:yellow;
+  }
+
+  a:hover {
+    color:darkorange;
+  }
+
+  a:visited {
+    color:teal;
+  }
+
+  a:visited:hover {
+    color:indigo;
+  }
+</style>
 </head>
 <body>
 <?php
