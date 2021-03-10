10b934228df0fb8b1b24e179957ec2a49ff1f8fe
monawasensei <75004614+monawasensei@users.noreply.github.com>
2021-03-09 18:53:31 -0600

    Update queue_handler.js

--- mosasa_dogs/queue_handler.js
+++ mosasa_dogs/queue_handler.js
@@ -232,7 +232,7 @@ function add_entry_to_subQueue_div(entry) {
 	entry.subQueueDivButton.setAttribute("id",entry.entryDivId + "-subQueue-div-btn");
 	entry.subQueueDivButton.setAttribute("class","entry-btn");
 	entry.subQueueDivButton.setAttribute("onclick","remove_entry_from_subQueue(\"" + entry.entryDivId + "\")");
-	entry.subQueueDivButton.appendChild(createTextNode(entry.lineData.title));
+	entry.subQueueDivButton.appendChild(document.createTextNode(entry.lineData.title));
 	var subQueueDiv = document.getElementById("subQueue-div");
 	subQueueDiv.appendChild(entry.subQueueDivButton);
 }
