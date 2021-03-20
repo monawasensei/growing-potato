831b35005e6007264dcd29667b8565ef57dcedc8
monawasensei@gmail.com <monawasensei@gmail.com>
2021-03-20 11:22:10 -0500

    changing the buttons and removing <a> tags

--- mosasa_dogs/index.php
+++ mosasa_dogs/index.php
@@ -28,6 +28,7 @@
 	</style>
 </head>
 <body>
+	<div id="top" style="display:none"></div>
 	<div id="log" style="display:none">
 	<?php
 
@@ -60,7 +61,7 @@
 			<div id="queue-control-div">
 				<button type="button" onclick="autoplay_next_entry()" class="btn-primary">Play Next</button>
 				<button type="button" onclick="shuffle_queue()" class="btn-primary">Shuffle</button>
-				<a href="https://holedigging.club/mosasa_dogs#bottom"><button type="button" class="btn-primary" id="top"></a> <!--Hope I can stick a button in an anchor tag, probably an easier way to do this anyways-->
+				<button type="button" class="btn-primary" onclick="location.hash=\"bottom\"">Bottom</button> <!--Hope I can stick a button in an anchor tag, probably an easier way to do this anyways-->
 				<!--
 				<button type="button" onclick="make_random_subQueue_10()" class="btn-primary">Random 10</button>
 				-->
@@ -75,6 +76,7 @@
 	</div>
 
 	<noscript>why don'd u hab jabasgribd durnd on? :DDDDDDDDDDDDDDDD</noscript>
-<a href="https://holedigging.club/mosasa_dogs#top"><button type="button" class="btn-primary" id="bottom"></a>
+<button type="button" class="btn-primary" onclick="location.hash=\"top\"">Top</button>
+<div id="bottom" style="display: none"></div>
 </body>
 </html>
