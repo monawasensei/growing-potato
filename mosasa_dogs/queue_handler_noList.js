58cea174a7a5b8cc3ec7b16ea304256cbb00affc
monawasensei <75004614+monawasensei@users.noreply.github.com>
2021-03-25 12:37:58 -0500

    Update queue_handler_noList.js

--- mosasa_dogs/queue_handler_noList.js
+++ mosasa_dogs/queue_handler_noList.js
@@ -279,7 +279,7 @@ function getListIndex(list,item) {
 function loadYoutubeIframeAPIScript() {
 	var tag = document.createElement('script');
 
-	tag.src = "https://www.youtube.com/iframe_api";
+	tag.src = "www.youtube.com/iframe_api";
       	var firstScriptTag = document.getElementsByTagName('script')[0];
       	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }
