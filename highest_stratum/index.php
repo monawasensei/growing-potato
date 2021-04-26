4b397fd54781337dcc648bb1b631ceab8e6c28df
monawasensei <monawasensei@gmail.com>
2021-04-26 14:36:17 -0500

    Update index.php

--- highest_stratum/index.php
+++ highest_stratum/index.php
@@ -9,48 +9,6 @@
 	<body>
 		<div class="essay">
             <?php
-
-                function parseTitle($fileString) {
-                    $regexPattern = "T";
-                    $fileString = "<h1 style=\"text-align: center\">" . $fileString;
-                    $fileString = preg_replace($regexPattern, "B");
-                    return $fileString;
-                }
-                
-                function parseParagraphs($fileString) {
-                    $regexPattern = "\t";
-                    //$fileString = "<h1 style=\"text-align:center\">" . $fileString;
-                    $count = preg_match_all($regexPattern, $fileString);
-                    for ($i = 0; $i <= $count; $i++) {
-                        if ($i == 0) {
-                            $fileString = preg_replace("\t", "<p class=\"essay-text\">", $fileString);
-                        } else {
-                            $fileString = preg_replace("\t", "<br></p><p class=\"essay-text\">", $fileString);
-                        }
-                    }
-                    $fileString = $fileString . "</p>";
-                    return $fileString;
-                }
-/*
-                function parseQuotes($fileString) { //will need to use regex for this probably //will not work if there are non-quote quotation marks elsewhere in the document
-                    $count = substr_count($fileString, "\"");
-                    for ($i = 0; $i <= $count; $i++) {
-                        if ($i == 0) {
-                            $fileString = str_replace("\'", "<div class=\"")
-                        } else {
-
-                        }
-                    }
-                    $fileString = $fileString . "</div>";
-                }
- */               
-                function parseFile($file,$filepath) {
-                    $fileString = fread($file, filesize($filepath));
-                    //$fileString = parseTitle($fileString);
-                    //$fileString = parseParagraphs($fileString);
-                    echo $fileString;
-                }
-
                 function getFile() {
                     $filename = $_GET['post'];
                     $filepath = "writings/" . $filename;
@@ -61,6 +19,6 @@
                 getFile();
             ?>
 		</div>
-        <a href="?post=testParse.txt">test</a>
+        <a href="?post=highestStratumExample.html">test</a>
 	</body>
 </html> 
