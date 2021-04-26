e9c18f634f588d6ac6cb98fdc66ef37bf779ae31
monawasensei <monawasensei@gmail.com>
2021-04-25 19:29:21 -0500

    Create index.php
    creating a simple php function to read the contents of the cwd and echo the names of directories contained in it.

---
+++ highest_stratum/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Highest Stratum</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<meta charset="utf-8"> <!-- need to pay more attention to syntax and learn how meta tags work -->
+		<link rel="stylesheet" href="stylesheet.css">
+	</head>
+	<body>
+		<div class="essay">
+            <?php
+                echo "desding sum bhb :DDD";
+
+                function getArticles() {
+                    $currentDir = dir(getcwd());
+                    $currentDirContents = scandir($currentDir);
+                    foreach ($currentDirContents as $item) {
+                        if (is_dir($item)) {
+                            echo $item; //I think this will only echo "writings"
+                        }
+                    }
+                }
+
+                getArticles();
+            ?>
+			<div class="essay-quote">
+				<p> 
+				If only we didn't have to eat to live,<br>	
+				I would hardly... call that living<br>
+				- Titus and Julius (1,5,3)<br><br>
+				</p>
+			</div>
+			<div class="link-to-essay">
+				<a href="writings/BeowulfAndGrendel.html">Go to the sample essay</a>
+			</div>
+		</div>
+	</body>
+</html> 
