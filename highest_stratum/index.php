cdf949b873d82c830fcae98915ddfa06f74d7608
monawasensei <monawasensei@gmail.com>
2021-04-25 19:44:03 -0500

    Update index.php

--- highest_stratum/index.php
+++ highest_stratum/index.php
@@ -12,7 +12,7 @@
                 echo "desding sum bhb :DDD";
 
                 function getArticles() {
-                    $currentDir = dir("/writings/");
+                    $currentDir = dir(getcwd());
                     echo "currentDir" . $currentDir->handle . "<br>" . $currentDir->path . "<br>";
                     $currentDirContents = scandir($currentDir);
                     //echo "currentDirContents" . $currentDirContents;
