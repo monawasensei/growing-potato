c30fb77b64a1558b31f9d0083edb41ecd74f86b0
monawasensei <monawasensei@gmail.com>
2021-04-25 20:05:50 -0500

    Update index.php

--- highest_stratum/index.php
+++ highest_stratum/index.php
@@ -9,26 +9,20 @@
 	<body>
 		<div class="essay">
             <?php
-                $filepath = "writings/testParse.txt";
-                $file = fopen($filepath, "r") or die("could not open file");
-
+                
                 function parseFile($file,$filepath) {
                     echo fread($file, filesize($filepath));
                 }
 
-                parseFile($file, $filepath);
+                function getFile() {
+                    $filename = $_GET['post'];
+                    $filepath = "writings/" . $filename;
+                    $file = fopen($filepath, "r") or die("could not open file");
+                    parseFile($file, $filepath);
+                }
 
+                getFile();
             ?>
-			<div class="essay-quote">
-				<p> 
-				If only we didn't have to eat to live,<br>	
-				I would hardly... call that living<br>
-				- Titus and Julius (1,5,3)<br><br>
-				</p>
-			</div>
-			<div class="link-to-essay">
-				<a href="writings/BeowulfAndGrendel.html">Go to the sample essay</a>
-			</div>
 		</div>
 	</body>
 </html> 
