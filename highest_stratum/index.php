51675401f2af7c3a4129bf41c6b8b17a404c2e8c
monawasensei <monawasensei@gmail.com>
2021-05-13 18:53:36 -0500

    added shiny tiner
    shiny tiner

--- highest_stratum/index.php
+++ highest_stratum/index.php
@@ -16,6 +16,8 @@
             <br>
 			<a href="writings/imabokuwa.html" class="link-to-essay">Shidbosd :DD</a>
             <br>
+			<a href="writings/shinitaina.html" class="link-to-essay">Yelling is good for you.</a>
+            <br>
 		</div>
 	</body>
 </html> 
